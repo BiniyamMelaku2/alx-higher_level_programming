9306f446d3557bd96e3ba9b3369d864fd952511c
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-04-27 13:32:18 +0000

    prints combinations of 2digit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for numb1 in range(0, 10):
      for numb2 in range(numb1 + 1, 10):
          print("{:d}{:d}".format(numb1, numb2),
-               end="\n" if numb1 == 8 and numb2 == 9 else ", ")
+             end="\n" if numb1 == 8 and numb2 == 9 else ", ")

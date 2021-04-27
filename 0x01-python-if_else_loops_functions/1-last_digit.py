34afd6f5a527150c6420254f356f24cc92bf23fb
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-04-27 12:41:48 +0000

    checks sign of last digit of random number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,9 +8,9 @@ else:
 if ldigit > 5:
     print("Last digit of {} is {} and is greater than 5"
            .format(number, ldigit))
-if ldigit is 0:
+elif ldigit is 0:
     print("Last digit of {} is {} and is 0"
           .format(number, ldigit))
-if ldigit < 6:
+elif ldigit < 6 and not 0:
     print("Last digit of {} is {} and is less than 6 and not 0"
           .format(number, ldigit))

b95dace214fc01e7bc63a6924e90753c589a0627
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-05-04 16:01:19 +0000

    removes all characters c and C from a string.

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def no_c(my_string):
+    new_string = ""
+    for str in my_string:
+        if str != 'c' and str != 'C':
+            new_string += str
+    return (new_string)

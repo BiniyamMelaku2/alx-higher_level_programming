a876dcea1772ae399b074424de4044898edcc023
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-05-18 18:17:01 +0000

    python BYTECODE generator MagicClass

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""Class docstring"""
-import math
 import dis
+import math
 
 
 class MagicClass:
     def __init__(self, radius=0):
-        """Init docstring
-        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-                raise TypeError('radius must be a number')
+            raise TypeError('radius must be a number')
         self.__radius = radius
 
     @property
     def radius(self):
         return self.__radius
-    
+
     def area(self):
-        """Area dostring"""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        """Circumference docstring"""
         return 2 * math.pi * self.__radius

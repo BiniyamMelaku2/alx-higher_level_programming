8a9a74e7923c01428e401495e3bdc862e3e52536
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-05-18 19:32:14 +0000

    class with getter and setter methods defined

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""
-This is the "Square"  class module
-"""
+""" Square class with get set methods defined """
 
 
 class Square:
-    """A class that defines a square by size, which defaults 0.
-    """
+    """ constructor method """
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
@@ -16,12 +13,12 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
-        if type(size) != int:
+    def size(self, value):
+        if type(value) != int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     @property
     def position(self):
@@ -32,27 +29,37 @@ class Square:
         if type(value) != tuple or len(value) != 2 or \
            not all([type(i) == int for i in value]):
             raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+           raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def __repr__(self):
-        return (self.get_str())
+        newstr = ""
+        if self.__size > 0:
+            for row in range(self.__position[1]):
+                newstr += "\n"  
+            for row in range(self.__size):
+                for col in range(self.__position[0]):
+                    newstr += " "
+                for col in range(self.__size):
+                    newstr += "#"
+                newstr += "\n"
+        else:
+            newstr += '\n'
+        return newstr[:-1]
 
     def area(self):
         return self.__size * self.__size
 
-    def get_str(self):
-        total = ""
-        if self.__size is 0:
-            total += "\n"
-            return total
-        for i in range(self.__position[1]):
-            total += "\n"
-        for i in range(self.__size):
-            total += (" " * self.__position[0])
-            total += ("#" * self.__size)
-            if i is not (self.__size - 1):
-                total += "\n"
-        return total
-
     def my_print(self):
-        print(self.get_str())
+        if self.__size > 0:
+            for row in range(self.__position[1]):
+                print()  
+            for row in range(self.__size):
+                for col in range(self.__position[0]):
+                    print(" ", end="")
+                for col in range(self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print()

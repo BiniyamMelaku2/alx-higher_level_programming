b5a3c16c43620d82a242d16d2fdc5e5c99911303
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-05-18 16:26:57 +0000

    constructor instantiates private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square():
+    """class constructor"""
+
+
+    def __init__(self, size):
+        """private attribute"""
+        self.__size = size

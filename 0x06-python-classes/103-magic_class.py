c3aed0e7769d1fe63be662b902743f1005ac6ca2
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-05-18 17:55:21 +0000

    python BYTECODE generator MagicClass

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,10 +5,11 @@ import math
 
 class MagicClass:
     def __init__(self, radius=0):
-        """Init docstring"""
+        """Init docstring
+        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-                raise TypeError("radius must be a number")
+                raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):

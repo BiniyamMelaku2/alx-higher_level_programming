042d6fa1fe6dec0bc3a2178d15e2cd53974c3eda
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-05-18 16:19:56 +0000

    defines an empty class square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" an empty class square """
+"""an empty class square"""
 
 
-class square:
-    """ defines an empty class square """
+class square():
+    """defines an empty class square"""
     pass

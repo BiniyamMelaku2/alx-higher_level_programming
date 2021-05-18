505572f2b3eb87e4a12347bfc7dcbea2c656b7d8
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-05-18 19:39:01 +0000

    class with getter and setter methods defined

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -26,11 +26,12 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) != tuple or len(value) != 2 or \
-           not all([type(i) == int for i in value]):
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) != int or type(value[1]) != int:
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
-           raise TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def __repr__(self):

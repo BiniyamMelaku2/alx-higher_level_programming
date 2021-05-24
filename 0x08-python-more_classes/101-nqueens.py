d18271eef961a57ad13beff10a3db6c94c00a2d1
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-05-24 20:44:12 +0000

    The N queens puzzle

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,62 +1,55 @@
 #!/usr/bin/python3
-""" a Module for N Queens Problem """
+"""
+nqueens backtracking program to print the coordinates of n queens
+"""
 
 
-def check_queen(board, num):
-    for i in range(len(board)):
-        if num == board[i][1]:
-            return True
-        return False
-
-
-def reject(board, col, num):
-    if check_queen(board, num):
-        return False
-    i = 0
-    while i < col:
-        if abs(board[i][1] - num) == abs(i - col):
-            return False
-        i += 1
-    return True
-
-
-def clear_moves(board, col):
-    """ clear board not safe """
-
-    for i in range(col, len(board)):
-        board[i][1] = None
-
-
-def checkBoard(board, col):
-    """ checks board by backtracking in every box state """
-
-    for num in range(len(board)):
-        clear_moves(board, col)
-        if reject(board, col, num):
-            board[col][1] = num
-            if (col == len(board) - 1):
-                print(board)
-            else:
-                checkBoard(board, col + 1)
+from sys import argv
 
 if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) != 2:
+    a = []
+    if len(argv) != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
-    num = 0
-    try:
-        num = int(sys.argv[1])
-    except:
+        exit(1)
+    if argv[1].isdigit() is False:
         print("N must be a number")
-        sys.exit(1)
-    if num < 4:
+        exit(1)
+    n = int(argv[1])
+    if n < 4:
         print("N must be at least 4")
-        sys.exit(1)
+        exit(1)
 
-    board = []
-    for i in range(num):
-        board.append([i, None])
+    for i in range(n):
+        a.append([i, None])
 
-    checkBoard(board, 0)
+    def already_exists(y):
+        for x in range(n):
+            if y == a[x][1]:
+                return True
+        return False
+
+    def reject(x, y):
+        if (already_exists(y)):
+            return False
+        i = 0
+        while(i < x):
+            if abs(a[i][1] - y) == abs(i - x):
+                return False
+            i += 1
+        return True
+
+    def clear_a(x):
+        for i in range(x, n):
+            a[i][1] = None
+
+    def nqueens(x):
+        for y in range(n):
+            clear_a(x)
+            if reject(x, y):
+                a[x][1] = y
+                if (x == n - 1):  
+                    print(a)
+                else:
+                    nqueens(x + 1)  
+
+    nqueens(0)

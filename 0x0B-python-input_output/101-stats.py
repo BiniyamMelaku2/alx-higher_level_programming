f1464bed0620aa3107d36526adf55f02ea97e2a4
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-06-01 19:26:06 +0000

    reads stdin line by line and computes stats metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -10,31 +10,32 @@ def print_stats(file_size, dict):
         if dict[key] != 0:
             print("{}: {}".format(key, dict[key]))
 
-if __name__ == "__main__":
-    file_size = 0
-    count = 0
-    dict = {'200': 0, '301': 0, '400': 0, '401': 0,
-            '403': 0, '404': 0, '405': 0, '500': 0}
 
-    try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(file_size, dict)
-                count = 1
-            else:
-                count += 1
+"""declaring variables"""
+file_size = 0
+count = 0
+dict = {'200': 0, '301': 0, '400': 0, '401': 0,
+        '403': 0, '404': 0, '405': 0, '500': 0}
 
-            line_split = line.split()
-            file_size += int(line_split[-1])
+""" Read from stdin """
+try:
+    for line in sys.stdin:
+        if count == 10:
+            print_stats(file_size, dict)
+            count = 1
+        else:
+            count += 1
 
-            try:
-                if line_split[-2] in dict:
-                    dict[line_split[-2]] += 1
-            except IndexError:
-                pass
+        line_split = line.split()
+        file_size += int(line_split[-1])
 
-        print_stats(file_size, dict)
-    except KeyboardInterrupt:
-        print_stats(file_size, dict)
-        raise
+        try:
+            if line_split[-2] in dict:
+                dict[line_split[-2]] += 1
+        except IndexError:
+            pass
+
     print_stats(file_size, dict)
+except KeyboardInterrupt:
+    print_stats(file_size, dict)
+    raise

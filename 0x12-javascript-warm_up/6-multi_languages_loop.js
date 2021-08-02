d760168921a9208879dabb2bf3fba397abff6271
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-08-02 13:45:32 +0000

    prints 3 lines using an array of string and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 // a script that prints 3 lines using an array of string and a loop
 const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const item in myArray)
+for (const item in myArray) {
   console.log(myArray[item]);
+}

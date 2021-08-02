067c73c948e7169ea14f49ee22bd1ccd2c62e121
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-08-02 13:56:18 +0000

    prints x C is funtimes

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@
 const process = require('process');
 const num = Math.floor(Number(process.argv[2]));
 if (isNaN(num)) {
-  consol.log();
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < num; i++) {
     console.log('C is fun');

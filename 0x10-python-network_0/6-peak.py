e175939aae71d04ab0450bfd0e2ab7f338b5cd64
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-08-19 22:15:21 +0000

    Test function find_peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" Test function find_peak """
+""" function find_peak """
 
 
 def find_peak(list_of_integers):
+    '''finds peak'''
     if len(list_of_integers) > 0:
         list_of_integers.sort()
         return list_of_integers[-1]

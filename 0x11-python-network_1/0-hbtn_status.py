d071c688695010079ef87901f55dac5f4878c4e8
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-08-20 12:13:40 +0000

    script that fetches https

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         charset = html.decode('utf-8')
-        print("Body response")
+        print("Body response:")
         print(
             "\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
             .format(type(html), html, charset))

91dd889583a39a487f0e31ed6798f9c5e5364f99
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-08-20 19:39:39 +0000

    fetches a url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 if __name__ == "__main__":
     html = requests.get("https://intranet.hbtn.io/status")
-    print("Body response")
+    print("Body response:")
     print(
         "\t- type: {}\n\t- content: {}"
         .format(type(html.text), html.text))

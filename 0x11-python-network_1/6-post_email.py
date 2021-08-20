3375a0aa080298e202d9353892cad343e52e8257
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-08-20 19:41:47 +0000

    sends a POST request to the passed URL

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,5 +5,5 @@ import requests
 
 
 if __name__ == "__main__":
-    mail = requests.post(sys.argv[1], data=('email': sys.argv[2]))
+    mail = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(mail.text)

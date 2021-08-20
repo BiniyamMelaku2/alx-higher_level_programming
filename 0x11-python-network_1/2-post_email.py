894f06c4e4c5c7703deaf7d1dd705d9769be170b
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-08-20 13:14:18 +0000

    takes in a URL and an email, sends a POST request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,6 +8,6 @@ from urllib import parse
 if __name__ == "__main__":
     post_data = parse.urlencode({'email': sys.argv[2]}).encode('ascii')
     with urllib.request.urlopen(url=sys.argv[1], data=post_data) as response:
-        data = post_response.read()
+        data = response.read()
         data = data.decode('ascii')
         print(data)
